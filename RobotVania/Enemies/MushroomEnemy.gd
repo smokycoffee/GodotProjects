556692fd1d682099b583fb22ee0e09db41b40a2c
Tsenguun <Tsenguun.batbold12@gmail.com>
2022-08-20 14:11:07 +0200

    crawling enemy

---
+++ RobotVania/Enemies/MushroomEnemy.gd
@@ -0,0 +1,16 @@
+extends "res://Enemies/PlantEnemy.gd"
+
+
+#const EnemyBullet = preload("res://Player/EnemyBullet.tscn")
+
+#export(int) var BULLET_SPEED = 30
+#export(int) var SPREAD = 30
+
+#onready var fireDirection = $FireDirection
+#onready var bulletSpawnPoint = $BulletSpawnPoint
+
+func fire_bullets():
+	var bullet = Utils.instance_scene_on_main(EnemyBullet, bulletSpawnPoint.global_position)
+	var velocity = (fireDirection.global_position - global_position).normalized() * BULLET_SPEED
+	velocity = velocity.rotated(deg2rad(rand_range(-SPREAD/2, SPREAD/2)))
+	bullet.velocity = velocity
